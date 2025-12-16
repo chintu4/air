407239de28365bc94f3df2792dec54a8b30dd83b
chintu4 <chintusharma00014@gmail.com>
2025-12-16 11:44:58 +0530

    autocommit

--- e.ps1
+++ e.ps1
@@ -1,10 +1,30 @@
-if ($args -eq "push"){
-    git add .
-    git commit -m "autocommit"
-    git push
-}
-if ($args -eq "pull"){
-    git add .
-    git commit -m "pull commit"
-    git pull origin master
+param(
+    [string]$cmd,
+    [string]$value
+)
+
+switch ($cmd) {
+    "push" {
+        git add .
+        git commit -m "autocommit"
+        git push
+    }
+
+    "pull" {
+        git pull origin master
+    }
+
+    "pr" {
+        if (-not $value) {
+            Write-Error "PR number required"
+            exit 1
+        }
+        echo "looking at pr $value"
+        gh pr ready $value
+        gh pr merge $value --merge --delete-branch
+    }
+
+    default {
+        Write-Error "Unknown command"
+    }
 }
